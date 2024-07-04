451d5eec571d81adf8c65a899b219e67ae47f0f7
username8964 <163640899+username8964@users.noreply.github.com>
2024-07-05 04:17:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: 0BSD
 
-const doh = 'https://security.cloudflare-dns.com/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query'
+const doh = 'https://5w9qbegf0u.cloudflare-gateway.com/dns-query'
+const dohjson = 'https://5w9qbegf0u.cloudflare-gateway.com/dns-query'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 const r404 = new Response(null, {status: 404});
